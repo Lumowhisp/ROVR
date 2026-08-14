bd0363f8b7497937c873aee6fb7502f4b2dd65fe
Lumowhisp <kumaraditya12981006@gmail.com>
2026-08-15 01:05:14 +0530

    feat: add weight onboarding screen

---
+++ frontend/src/app/(onboarding)/WeightScreen/Weight.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+const BG = "#C8F3E5";
+
+export default function WeightScreen() {
+  return (
+    <>
+      <View style={{ backgroundColor: BG, flex: 1 }}></View>
+    </>
+  );
+}
